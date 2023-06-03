66ebe62985386870a6e79c8ec7e6e788a1618453
Yongjung Shin <yongshn220@gmail.com>
2023-06-03 20:12:41 +0900

    Add | Content Page

---
+++ screens/ContentScreen.js
@@ -0,0 +1,147 @@
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {signOut} from 'firebase/auth'
+import {useNavigation} from '@react-navigation/native';
+import {errorHandler} from '../errors';
+import {auth, db} from '../firebase';
+import {getDocs, collection} from "firebase/firestore";
+import styled from "styled-components/native";
+import {flexCenter, TonicButton} from "../utils/styleComponents";
+import {windowHeight, windowWidth} from "../utils/utils";
+import Post from "../components/Post";
+import {Center, FlatList, Input, Icon, Divider, Button} from "native-base";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+// import { getUsername } from '../firestore';
+
+
+export default function ContentScreen(props) {
+    const navigation = useNavigation()
+    const [currUser, setCurrUser] = useState(null);
+
+    useEffect(() => {
+        setCurrUser(auth.currentUser.displayName);
+    }, [])
+
+
+    // const getUsername = async () => {
+    //     console.log("finding username...");
+    //     const userCollection = collection(db, 'users');
+    //     const userSnap = await getDocs(userCollection);
+    //     userSnap.forEach(doc => {
+    //         if(doc.data().uid === auth.currentUser.uid) {
+    //             setUsername(doc.data().username);
+    //             console.log("set username..")
+    //         }
+    //     });
+    // }
+    // getUsername();
+
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                navigation.replace("LoginNavigator")
+                console.log(`${username} logged out`)
+            })
+            .catch(error => alert(errorHandler(error)))
+    }
+
+    const LoadingView = <View><Text>Loading...</Text></View>
+
+    const postList = [
+        {id: 1},
+        {id: 2},
+        {id: 3},
+        {id: 4},
+        {id: 5},
+        {id: 6},
+        {id: 7},
+        {id: 8},
+        {id: 9},
+        {id: 10},
+        {id: 11},
+        {id: 12},
+        {id: 13},
+        {id: 14},
+        {id: 15},
+    ]
+    const SearchBar = (<Input placeholder="Search" variant="filled" width="90%" borderRadius="50" py="3" px="2" InputLeftElement={<Icon ml="2" size="5" color="gray.400" as={<Ionicons name="ios-search"/>}/>}/>)
+
+    const ContentView = (
+        <Center flex={1} px="0">
+            <FlatList
+                data={postList}
+                renderItem={(post) => {
+                    return (
+                        <View style={{padding: 10}}>
+                            <Post onClickHandler={handleContentClick} key={post.id}/>
+                        </View>
+                    );
+                }}
+                alwaysBounceVertical={false}
+            />
+        </Center>
+    )
+
+    const MainView = !currUser ? LoadingView : ContentView
+
+    function handleContentClick() {
+        props.navigation.navigate("ContentDetailNavigator")
+    }
+
+    function handleCreateButtonClick() {
+
+    }
+    return (
+        <Container>
+            <SafeAreaView>
+                <Center>
+                    {SearchBar}
+                </Center>
+                <Divider style={{marginTop: 20}}/>
+            </SafeAreaView>
+            <ContentArea>
+                {MainView}
+            </ContentArea>
+            <CreateButtonArea>
+                <TouchableOpacity
+                    onPress={handleCreateButtonClick}
+                >
+                    <CreateButton>
+                        <BasicText>+</BasicText>
+                    </CreateButton>
+                </TouchableOpacity>
+            </CreateButtonArea>
+        </Container>
+    )
+}
+
+const Container = styled.View`
+  display: flex;
+  flex: 1;
+  background-color: #fff;
+`;
+
+const CreateButtonArea = styled.View`
+  position: absolute;
+  top: ${windowHeight - 150};
+  left: ${windowWidth - 80};
+`
+const CreateButton = styled.View`
+  ${TonicButton};
+  border-radius: 100px;
+  width: 60px;
+  height: 60px;
+`;
+
+const ContentArea = styled.View`
+  display: flex;
+  flex: 1;
+  background-color: #fff;
+`;
+
+const BasicText = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+`;
