741e6246351e1ab87c4a26d6d9caea50d949c91d
Yongjung Shin <yongshn220@gmail.com>
2023-06-03 22:51:28 +0900

    Add Content Page & Posting Page

--- navigations/MainNavigator.js
+++ navigations/MainNavigator.js
@@ -3,16 +3,19 @@ import {View, Text} from 'react-native'
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack'
 import StartNavigator from "./StartNavigator"
 import HomeNavigator from './HomeNavigator'
-import ContentDetailNavigator from "./ContentDetailNavigator";
+import ContentDetailNavigator from "./ContentDetailNavigator"
+import PostingNavigator from './PostingNavigator'
+import {NavigatorType} from '../utils/utils.js'
 
 const Stack = createStackNavigator();
 
 export default function MainNavigator() {
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="LoginNavigator" component={StartNavigator}/>
-            <Stack.Screen name="HomeNavigator" component={HomeNavigator}/>
-            <Stack.Screen name="ContentDetailNavigator" component={ContentDetailNavigator}  options={{title: "", headerShown: false}}/>
+            <Stack.Screen name={NavigatorType.LOGIN} component={StartNavigator}/>
+            <Stack.Screen name={NavigatorType.HOME} component={HomeNavigator} options={{title: ""}}/>
+            <Stack.Screen name={NavigatorType.CONTENT_DETAIL} component={ContentDetailNavigator} options={{title: "", headerShown: true}}/>
+            <Stack.Screen name={NavigatorType.POSTING} component={PostingNavigator}/>
         </Stack.Navigator>
     )
 }
